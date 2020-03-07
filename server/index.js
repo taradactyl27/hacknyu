c00e3e5f35088618d436bb8ea6b23cb1b741251b
Andy Mina <andy.mina@macaulay.cuny.edu>
2020-03-06 19:34:15 -0500

    Made server

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const keys = require("./config/keys");
+
+const app = express();
+
+// Bodyparser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect(keys.MONGO_KEY, { useNewUrlParser: true }).then(() => {
+  console.log("MongoDB successfully connected"))
+}).catch((err) => {
+  console.log(err)
+});
+
+// run the server
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if we deploy
+app.listen(port, () => console.log(`Server running on port ${port}!`));
