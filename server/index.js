2ee16e140ce7728edf0f6d5f115f7bedbabd50e7
VladimirAndreev09 <VladimirAndreev09@myhunter.cuny.edu>
2020-03-07 02:03:40 -0500

    Initial

--- server/index.js
+++ server/index.js
@@ -25,6 +25,55 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 
+app.post('/dataSubmittedHW', function(req,res){
+  let data = req.body.data;
+  MyModel.find({ name: 'data' });
+  /*const vision = require('@google-cloud/vision');
+  const client = new vision.ImageAnnotatorClient();
+  
+  const fileName = 'Local image file, e.g. /path/to/image.png';
+
+  // Performs text detection on the local file
+  const [result] = await client.textDetection(fileName);
+  const detections = result.textAnnotations;
+  console.log('Text:');
+  detections.forEach(text => console.log(text));*/
+  
+})
+app.post('/dataSubmittedNotes', function(req,res){
+  let data = req.body.data;
+  /*const vision = require('@google-cloud/vision');
+
+  const client = new vision.ImageAnnotatorClient();
+  const fileName = 'Local image file, e.g. /path/to/image.png';
+  
+  // Read a local image as a text document
+  const [result] = await client.documentTextDetection(fileName);
+  const fullTextAnnotation = result.fullTextAnnotation;
+  console.log(`Full text: ${fullTextAnnotation.text}`);
+  fullTextAnnotation.pages.forEach(page => {
+    page.blocks.forEach(block => {
+      console.log(`Block confidence: ${block.confidence}`);
+      block.paragraphs.forEach(paragraph => {
+        console.log(`Paragraph confidence: ${paragraph.confidence}`);
+        paragraph.words.forEach(word => {
+          const wordText = word.symbols.map(s => s.text).join('');
+          console.log(`Word text: ${wordText}`);
+          console.log(`Word confidence: ${word.confidence}`);
+          word.symbols.forEach(symbol => {
+            console.log(`Symbol text: ${symbol.text}`);
+            console.log(`Symbol confidence: ${symbol.confidence}`);
+          });
+        });
+      });
+    });
+  });*/
+})
+
+app.post('/dataSubmittedNotes', function(req,res){
+  let data = req.body.data;
+
+})
 // run the server
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if we deploy
 app.listen(port, () => console.log(`Server running on port ${port}!`));
