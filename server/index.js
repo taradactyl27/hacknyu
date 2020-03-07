c0a52c2e02b49c44b4b85cddc0a7b8fad94eee90
Valentine-S <valentineshidlovskiy@gmail.com>
2020-03-06 23:47:46 -0500

    Server files

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const keys = require("./config/keys");
+const passport = require("passport");
 
+const users = require("./routes/api/users");
 const app = express();
 
 // Bodyparser middleware
@@ -15,6 +17,13 @@ mongoose.connect(keys.MONGO_KEY, { useNewUrlParser: true }).then(() => {
 }).catch((err) => {
   console.log(err);
 });
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
 
 // run the server
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if we deploy
