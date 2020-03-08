7064fd3e1edf325ae26c7702bb2ea17038d77d7a
Valentine-S <valentineshidlovskiy@gmail.com>
2020-03-07 20:10:57 -0500

    Update

--- server/api/TaskManager.js
+++ server/api/TaskManager.js
@@ -5,17 +5,6 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../config/keys");
 
-// add job
-addJob = (user, job) => {
-  // make id
-  let id = this.generateID();
-  // add user attached to this job
-  job.user = user.username;
-  // add job
-  this.jobs[id] = job;
-  // return id attached to job
-  return id;
-}
   router.post("create", (req,res) =>{
   let task = req.body.data;
   let id = req.body.id;
@@ -25,34 +14,13 @@ addJob = (user, job) => {
   )
   });
 
-// remove job
-// return true if successful
-removeJob = (id) => {
-  if (!this.validJob(id))
-    return false;
-  else {
-    this.jobs[id] = undefined;
-    return true;
-  }
-}
 
 router.delete("/deletetask", (req, res) => {
   res.send
 });
 
 
-// update the job
-// return true if success
-updateJob = (id, job) => {
-  if (!this.validJob(id)) {
-    // move auxilary information over
-    job.info = this.jobs[id].info;
-    this.jobs[id] = job;
-    return true;
-  }
 
-  return false;
-}
 
 router.put("/makeactive", (req, res) => {
   let task = req.body.data;
@@ -64,32 +32,5 @@ router.put("/makeactive", (req, res) => {
 });
 
 
-// returns job if found, otherwise false
-getJob = (id) => {
-  return this.jobs[id];
-}
-
-// returns true if job found, false otherwise
-validJob = (id) => {
-  return this.jobs[id] !== undefined;
-}
-
-// make ID for job
-generateID = () => {
-  const valid = "ABCDEFGHIJKLMNPQRSTUVWXYZ1234567890";
-  let id;
-
-  do {
-    id = "";
-
-    for (let i = 0; i < 4; i++){
-      const char = valid[Math.floor(Math.random() * valid.length)];
-      code += char;
-    }
-
-    if (this.validJob(code)) return code;
-  } while (this.validJob(code));
-}
-}
 
 module.exports = Router;
